66898c4ce0e12816b1a4bf9943b96b283b720977
Dee1234pak <89565631+Dee1234pak@users.noreply.github.com>
2023-01-31 16:44:10 +0530

    Add files via upload

---
+++ DETECTFACE.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+alg = ("C:\\Users\\Asus\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
+haar_cascade = cv2.CascadeClassifier(alg)
+cam = cv2.VideoCapture(0)
+while True:
+    _,img = cam.read()
+    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = haar_cascade.detectMultiScale(grayImg,1.3,4)
+
+    for (x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("FaceDetection",img)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
